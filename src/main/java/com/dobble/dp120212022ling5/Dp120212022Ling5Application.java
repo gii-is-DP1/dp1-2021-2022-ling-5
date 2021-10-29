764c601fdbfe98a1c11792fa06a41f738492a342
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-10-29 13:36:45 +0200

    prueba

--- src/main/java/com/dobble/dp120212022ling5/Dp120212022Ling5Application.java
+++ src/main/java/com/dobble/dp120212022ling5/Dp120212022Ling5Application.java
@@ -1,13 +1,13 @@
-package com.LIng5.Dobble;
+package com.dobble.dp120212022ling5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DobbleApplication {
+public class Dp120212022Ling5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DobbleApplication.class, args);
+		SpringApplication.run(Dp120212022Ling5Application.class, args);
 	}
 
 }
--- src/test/java/com/dobble/dp120212022ling5/Dp120212022Ling5ApplicationTests.java
+++ src/test/java/com/dobble/dp120212022ling5/Dp120212022Ling5ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.LIng5.Dobble;
+package com.dobble.dp120212022ling5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DobbleApplicationTests {
+class Dp120212022Ling5ApplicationTests {
 
 	@Test
 	void contextLoads() {
