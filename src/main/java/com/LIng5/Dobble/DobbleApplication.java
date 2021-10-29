a12cc62e9220b3053396444f69f136284b7237eb
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-10-29 09:43:06 +0200

    Proyecto en React

---
+++ src/main/java/com/LIng5/Dobble/DobbleApplication.java
@@ -0,0 +1,13 @@
+package com.LIng5.Dobble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LIng5/Dobble/DobbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LIng5.Dobble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
