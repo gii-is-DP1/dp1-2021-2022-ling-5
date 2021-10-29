605cc5dd9f32762d9ad25696240f4c2fcbddd8b2
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-10-29 14:57:22 +0200

    react

---
+++ src/test/java/com/dobble/dp120212022ling5/Dp120212022Ling5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobble.dp120212022ling5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dp120212022Ling5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
