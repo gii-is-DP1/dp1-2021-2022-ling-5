5df74c46c807a45d807cd4b2ce6f4c580e5091a8
Gregorio <greortsol@alum.us.es>
2021-11-13 15:21:33 +0100

    Cambio de cartas y js

---
+++ src/main/webapp/src/images/figures/figures.js
@@ -0,0 +1,118 @@
+import ajedrez from "./ajedrez.png"
+import ancla from "./ancla.png"
+import arana from "./arana.png"
+import arbol from "./arbol.png"
+import biberon from "./biberon.png"
+import bomba from "./bomba.png"
+import bombilla from "./bombilla.png"
+import bullseye from "./bullseye.png"
+import cactus from "./cactus.png"
+import calavera from "./calavera.png"
+import candado from "./candado.png"
+import cebra from "./cebra.png"
+import clave from "./clave.png"
+import copoNieve from "./copoNieve.png"
+import corazon from "./corazon.png"
+import delfin from "./delfin.png"
+import dragon from "./dragon.png"
+import exclamacion from "./exclamacion.png"
+import fantasma from "./fantasma.png"
+import gafas from "./gafas.png"
+import gato from "./gato.png"
+import gota from "./gota.png"
+import hielo from "./hielo.png"
+import hoguera from "./hoguera.png"
+import hoja from "./hoja.png"
+import iglu from "./iglu.png"
+import interrogacion from "./interrogacion.png"
+import labios from "./labios.png"
+import lapiz from "./lapiz.png"
+import llave from "./llave.png"
+import luna from "./luna.png"
+import manchas from "./manchas.png"
+import mano from "./mano.png"
+import manzana from "./manzana.png"
+import margarita from "./margarita.png"
+import mariquita from "./mariquita.png"
+import martillo from "./martillo.png"
+import muneco from "./muneco.png"
+import ojo from "./ojo.png"
+import pajaro from "./pajaro.png"
+import payaso from "./payaso.png"
+import perro from "./perro.png"
+import prohibido from "./prohibido.png"
+import queso from "./queso.png"
+import rayo from "./rayo.png"
+import relog from "./relog.png"
+import sol from "./sol.png"
+import taxi from "./taxi.png"
+import telarana from "./telarana.png"
+import tijeras from "./tijeras.png"
+import tortuga from "./tortuga.png"
+import trebol from "./trebol.png"
+import trex from "./trex.png"
+import us from "./us.png"
+import vela from "./vela.png"
+import yinyan from "./yinyan.png"
+import zanahoria from "./zanahoria.png"
+
+
+export default {
+ "ajedrez": ajedrez,
+ "ancla": ancla,
+ "arana": arana,
+ "arbol": arbol,
+ "biberon": biberon,
+ "bomba": bomba,
+ "bombilla": bombilla,
+ "bullseye": bullseye,
+ "cactus": cactus,
+ "calavera": calavera,
+ "candado": candado,
+ "cebra": cebra,
+ "clave": clave,
+ "copoNieve": copoNieve,
+ "corazon": corazon,
+ "delfin": delfin,
+ "dragon": dragon,
+ "exclamacion": exclamacion,
+ "fantasma": fantasma,
+ "gafas": gafas,
+ "gato": gato,
+ "gota": gota,
+ "hielo": hielo,
+ "hoguera": hoguera,
+ "hoja": hoja,
+ "iglu": iglu,
+ "interrogacion": interrogacion,
+ "labios": labios,
+ "lapiz": lapiz,
+ "llave": llave,
+ "luna": luna,
+ "manchas": manchas,
+ "mano": mano,
+ "manzana": manzana,
+ "margarita": margarita,
+ "mariquita": mariquita,
+ "martillo": martillo,
+ "muneco": muneco,
+ "ojo": ojo,
+ "pajaro": pajaro,
+ "payaso": payaso,
+ "perro": perro,
+ "prohibido": prohibido,
+ "queso": queso,
+ "rayo": rayo,
+ "relog": relog,
+ "sol": sol,
+ "taxi": taxi,
+ "telarana": telarana,
+ "tijeras": tijeras,
+ "tortuga": tortuga,
+ "trebol": trebol,
+ "trex": trex,
+ "us": us,
+ "vela": vela,
+ "yinyan": yinyan,
+ "zanahoria": zanahoria
+}
