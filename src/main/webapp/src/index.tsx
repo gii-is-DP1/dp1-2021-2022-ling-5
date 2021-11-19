1746f27570802d7a37481b883625d6d9f1152753
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-11-19 10:18:48 +0100

    Navbars

--- src/main/webapp/src/index.tsx
+++ src/main/webapp/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Links from './Links';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
