8f72a4a1299bf20eddd718c7132317f553da11d1
mancab <mancabmor1@alum.us.es>
2021-11-23 17:27:01 +0100

    Juego Terminado

--- src/main/webapp/src/index.tsx
+++ src/main/webapp/src/index.tsx
@@ -4,16 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Links from './Links';
-import OnGoingTest from './modules/games/OnGoingTorreInfernal/OnGoingTorreInfernalTest';
-import {GetCardTest, GetGameTest, GetPlayerCardTest, GetPointsTest} from './modules/games/OnGoingTorreInfernal/OnGoingTestGet';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GetGameTest />
-    <GetPlayerCardTest />
-    <GetCardTest />
-    <GetPointsTest />
-    <OnGoingTest />
     <Links />
   </React.StrictMode>,
   document.getElementById('root')
