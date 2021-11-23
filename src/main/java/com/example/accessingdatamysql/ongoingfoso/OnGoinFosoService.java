7e8e416898e82a1b4eef1c0252d9b5a2d038ba6e
greortsol <greortsol@gmail.com>
2021-11-24 00:17:52 +0100

    El foso 1

---
+++ src/main/java/com/example/accessingdatamysql/ongoingfoso/OnGoinFosoService.java
@@ -0,0 +1,55 @@
+package com.example.accessingdatamysql.ongoingfoso;
+
+import com.example.accessingdatamysql.card.Card;
+import com.example.accessingdatamysql.game.Game;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("singleton")
+public class OnGoinFosoService {
+    private static OnGoingFosoRepository ongoing = null;
+
+    public OnGoinFosoService(){
+        if(ongoing==null){
+            ongoing = new OnGoingFosoRepository();
+        }
+    }
+
+    public Iterable<OnGoingFoso> getAll(){
+        return ongoing.getAllOnGoingGames();
+    }
+
+    public void creatGame(Long gameId, Game game, Iterable<Card> cards){
+        ongoing.creatGame(gameId, game, cards);
+    }
+
+    public Card getPlayerCard(Long gameId, Long playerId){
+        return ongoing.getPlayerCard(gameId, playerId);
+    }
+
+    public Card getCenterCard(Long gameId){
+        return ongoing.getCenterCard(gameId);
+    }
+
+    public int getPoints(Long gameId, Long playerId){
+        return ongoing.getPoints(gameId, playerId);
+    }
+
+    public void changeCards(Long playerId,Long gameId){
+        ongoing.changeCards(playerId,gameId);
+    }
+
+    public void addPoints(Long gameId, Long playerId, Integer points){
+        ongoing.addPoints(gameId, playerId, points);
+    }
+
+    public OnGoingFoso getGame(Long gameId){
+        return ongoing.getGame(gameId);
+    }
+
+    public void deleteGame(Long gameId){
+        ongoing.deleteGame(gameId);
+    }
+}
---
+++ src/main/java/com/example/accessingdatamysql/ongoingfoso/OnGoingFoso.java
@@ -0,0 +1,47 @@
+package com.example.accessingdatamysql.ongoingfoso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import com.example.accessingdatamysql.card.Card;
+import com.example.accessingdatamysql.game.Game;
+import com.example.accessingdatamysql.user.Player;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OnGoingFoso{
+
+    private List<Player> players;
+    private List<Card> remainingCards;
+    private Map<Long, Card> playerCard;
+    private Map<Long, Integer> points;
+    private Card currentCard;
+    private Integer limit;
+
+    public OnGoingFoso(Long gameId, Game game, Iterable<Card> cards){ 
+        this.players = List.copyOf(game.getPlayers());
+        this.remainingCards = new ArrayList<Card>();
+        cards.forEach(card->remainingCards.add(card));
+        this.points = new HashMap<Long, Integer>();
+        this.playerCard = new HashMap<Long, Card>();
+        this.limit=remainingCards.size()/players.size();
+        for(Player player: this.players){
+            points.put(player.getId(), 0);
+            Random random = new Random();
+            int randomindex = random.ints(0, remainingCards.size()).findFirst().getAsInt();
+            Card card = remainingCards.get(randomindex);
+            remainingCards.remove(card);
+            playerCard.put(player.getId(), card);
+        }
+        Random random2 = new Random();
+        int randomindex2 = random2.ints(0, remainingCards.size()).findFirst().getAsInt();
+        this.currentCard = remainingCards.get(randomindex2);
+        remainingCards.remove(currentCard);
+    }
+}
