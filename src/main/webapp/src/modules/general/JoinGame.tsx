607e23e9f267656d7d67a9a041c278b35a160a8c
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-11-26 12:39:42 +0100

    Fixed Join Game

--- src/main/webapp/src/modules/general/JoinGame.tsx
+++ src/main/webapp/src/modules/general/JoinGame.tsx
@@ -10,53 +10,53 @@ function JoinGame() {
   const [namegame, setNamegame] = useState<string>();
   var idplayer = 1;
 
-  function joinGame(){
+  function joinGame() {
     const requestOptions = {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     }
-    
+
     return new Promise(function (resolve, reject) {
       fetch(`http://localhost:8080/api/games/names/${namegame}`, requestOptions)
-      .then(res => {
-        res.json().then((game:any) => {
-          const gameid = game.id;
-          const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' }
-          }
+        .then(res => {
+          res.json().then((gameSearched: any) => {
+            const gameid = gameSearched.id;
+            const requestOptions = {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' }
+            }
 
-          fetch(`/games/${gameid}/players/${idplayer}`, requestOptions).then(res => {
+            fetch(`/games/${gameid}/players/${idplayer}`, requestOptions).then(res => {
               resolve(res.json())
+            })
+              .catch(error => reject(console.error));
+            window.location.href = '/startGame'
           })
-          .catch(error => reject(console.error));
-          window.location.href = '/startGame'
-      })
-      .catch(error => reject(console.error))
-      })
+            .catch(error => reject(console.error))
+        })
     })
   }
 
   return (
-  
-    <div className="NewGame-header"> 
+
+    <div className="NewGame-header">
       <p>JOIN GAME</p>
       <Form>
         <Form.Group className="mb-3">
           <Form.Label>Game's name you want to join</Form.Label>
-          <Form.Control placeholder="Enter game's name" onChange={(e) => setNamegame(e.target.value )}/>
+          <Form.Control placeholder="Enter game's name" onChange={(e) => setNamegame(e.target.value)} />
         </Form.Group>
 
-        
+
         <Button className="Button" size="lg" variant="dark" onClick={() => joinGame()}>
           JOIN
         </Button>
       </Form>
     </div>
 
-      
+
   );
 }
 
 
-export default JoinGame ;
+export default JoinGame;
