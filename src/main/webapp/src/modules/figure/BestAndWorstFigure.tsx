569e951e6a5b86bfd1a4cf2020a9ed41018ed0aa
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2021-12-17 21:01:13 +0100

    Conflictos resueltos

--- src/main/webapp/src/modules/figure/BestAndWorstFigure.tsx
+++ src/main/webapp/src/modules/figure/BestAndWorstFigure.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import figures from '../../images/figures/figures.js';
+import figureImg from "../../images/figures/figures.js";
 
 function BestAndWorstFigure() {
-  let playerId = '1';
+  let playerId='1';
   const [state, setState] = useState<any>()
   useEffect(() => {
-    fetch("http://localhost:8080/api/players/" + playerId + "/playerfigures")
+    fetch("http://localhost:8080/api/statistics/maxminfigure/"+playerId)
       .then(res => {
         console.log(res.status)
         return res.json()
@@ -15,32 +16,23 @@ function BestAndWorstFigure() {
       .catch(console.error)
   }, [])
   if (!state) return <div>Loading...</div>
-  let best = state[0];
-  let worst = state[0];
-  console.log(best.succesful)
-  for (let i = 0; i < state.length; i++) {
-    console.log(state[i].succesful)
-    if (best.succesful < state[i].succesful) {
-      best = state[i];
-    }
-    if (worst.succesful > state[i].succesful) {
-      worst = state[i];
-    }
-  }
+  console.log(state)
+    let best=state[0];
+    let worst=state[1];
   return (
     <div>
-      <Row>
-        <Col>
-          <h1>Most used:</h1>
-          <img src={figures(best.figure.id - 1)} width='50px' height='50px' />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h1>Least used:</h1>
-          <img src={figures(worst.figure.id - 1)} width='50px' height='50px' />
-        </Col>
-      </Row>
+        <Row>
+            <Col>
+                <h1>Most used:</h1>
+                <img src={figures(best.id-1)} width='50px' height='50px' />
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <h1>Least used:</h1>
+                <img src={figures(worst.id-1)} width='50px' height='50px' />
+            </Col>
+        </Row>
     </div>
   );
 }
--- src/main/webapp/src/modules/playedGames/userPoints.tsx
+++ src/main/webapp/src/modules/playedGames/userPoints.tsx
@@ -3,74 +3,34 @@ import { Col, Row } from 'react-bootstrap';
 import resultAPI from '../result/resultAPI';
 
 function UserPoints(props: any) {
-    const playerId: any = 1;
-    const [results, setResutls] = useState<any[]>([]);
-    useEffect(() => {
-      fetch(`http://localhost:8080/api/statistics/pointsbyminigames/${playerId}`)
-        .then(res => {
-            return res.json()
-        })
-        .then(data => setResutls(data))
-        .catch(console.error)
-  
-    }, [])
-    if (!results) return <div>Loading...</div>
-    let total=results[0];
-    let minigame1=results[1];
-    let minigame2=results[2];
-    let minigame3=results[3];
+  const playerId: any = 1;
+  const [results, setResutls] = useState<any[]>([]);
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/statistics/pointsbyminigames/${playerId}`)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => setResutls(data))
+      .catch(console.error)
 
-    return (
-      <div>
+  }, [])
+  if (!results) return <div>Loading...</div>
+  let total = results[0];
+  let minigame1 = results[1];
+  let minigame2 = results[2];
+  let minigame3 = results[3];
+
+  return (
+    <div>
       <Row>
         <Col>Total: {total}</Col>
       </Row>
       <Row>
-      <Col>Torre Infernal: {minigame1}</Col>
-      <Col>Foso: {minigame2}</Col>
-      <Col>Regalo Envenenado: {minigame3}</Col>
-    </Row>
+        <Col>Torre Infernal: {minigame1}</Col>
+        <Col>Foso: {minigame2}</Col>
+        <Col>Regalo Envenenado: {minigame3}</Col>
+      </Row>
     </div>
-    );
-  }
-  
-  
-  export default UserPoints;
-  
-  const playerId: any = 1;
-  const [results, setResults] = useState<any[]>([]);
-
-  useEffect(() => {
-    resultAPI.getAllResultsByPlayer(playerId)
-      .then((res: any[]) => setResults(res))
-      .catch((err) => console.log(err));
-  }, [])
-
-  if (!results) return <div>Loading...</div>
-  return (
-    <Row>
-      {
-        results.map(res => (
-          <Row>
-            <Col><h1>{res.game.name}</h1></Col>
-            {res.game.minigames.length > 1
-              ? [0, 1, 2].map(i => (
-                <Col>
-                  <h2>{res.game.minigames[i].name}</h2>
-                  <h3>{res.data.split(" ")[i]}</h3>
-                </Col>
-              ))
-              : <Col>
-                <h2>{res.game.minigames[0].name}</h2>
-                <h2>{res.game.minigames[0].name}</h2>
-                <h2>{res.game.minigames[0].name}</h2>
-                <h3>{res.data}</h3>
-              </Col>
-            }
-          </Row>
-        ))
-      }
-    </Row>
   );
 }
 
