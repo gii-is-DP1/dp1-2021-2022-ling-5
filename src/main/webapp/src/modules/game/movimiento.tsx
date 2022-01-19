8eba97e9d8e2212a298ae129a3809b58b61f150f
greortsol <greortsol@gmail.com>
2022-01-19 01:42:21 +0100

    Anadir punto al hacer comprobacion

--- src/main/webapp/src/modules/game/movimiento.tsx
+++ src/main/webapp/src/modules/game/movimiento.tsx
@@ -1,10 +1,38 @@
-export default function movimiento(id: any, card: any) {
+import { useEffect, useState } from "react";
+
+export default function movimiento(figureId: any, card: any) {
+    const [playerId, setPlayerId] = useState<number>();
     let res = false
     let figs = card.figures
-    console.log(figs)
-    for (var fig of figs) {
-        res = res || fig.id === id
+    useEffect(() => {
+        var userData: any = localStorage.getItem("userData");
+            var id = 0;
+            if (userData !== null) {
+                userData = JSON.parse(userData);
+                if (userData !== null) {
+                    setPlayerId(userData.id)
+                    id = userData.id
+                }
+        }
+    if (playerId !== 0) {
+        for (var fig of figs) {
+            res = res || fig.id === figureId
+        }
+    
+        if(res){
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: ''
+            }
+            fetch(`http://localhost:8080/api/player/${playerId}/figure/${figureId}/add`, requestOptions)
+                .then((res: any) => {
+                    console.log(`One point added to figure ${figureId} of player ${playerId}`)
+                }).catch((err: any) => console.log(err));
+        }
     }
+
+  }, [])
 
     return res;
 }
