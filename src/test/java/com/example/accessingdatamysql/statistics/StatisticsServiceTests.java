cef101c13db755fd24db02b2b986e9dd61627e13
Irene <i.dominguez@alumno.laudealtillo.com>
2022-01-20 14:53:29 +0100

    forum

--- src/test/java/com/example/accessingdatamysql/statistics/StatisticsServiceTests.java
+++ src/test/java/com/example/accessingdatamysql/statistics/StatisticsServiceTests.java
@@ -26,121 +26,121 @@ public class StatisticsServiceTests {
     @Autowired
     private FigureRepository figureRepository;
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testPointsByMinigames() {
+    @Test
+    @Transactional(readOnly = true)
+    void testPointsByMinigames() {
 
-    // List<Integer> l1 = new ArrayList<Integer>();
-    // l1.add(30);
-    // l1.add(10);
-    // l1.add(10);
-    // l1.add(10);
-    // assertEquals(statisticsService.pointsByMinigames(1L), l1);
-    // }
+        List<Integer> l1 = new ArrayList<Integer>();
+        l1.add(30);
+        l1.add(10);
+        l1.add(10);
+        l1.add(10);
+        assertEquals(statisticsService.pointsByMinigames(1L), l1);
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testMaxMinFigures() {
+    @Test
+    @Transactional(readOnly = true)
+    void testMaxMinFigures() {
 
-    // List<Figure> res = new ArrayList<Figure>();
-    // Figure f1 = figureRepository.findById(17L).get();
-    // Figure f2 = figureRepository.findById(52L).get();
-    // res.add(f1);
-    // res.add(f2);
-    // assertEquals(statisticsService.maxMinFigures(2L), res);
+        List<Figure> res = new ArrayList<Figure>();
+        Figure f1 = figureRepository.findById(17L).get();
+        Figure f2 = figureRepository.findById(52L).get();
+        res.add(f1);
+        res.add(f2);
+        assertEquals(statisticsService.maxMinFigures(2L), res);
 
-    // }
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testRanking() {
+    @Test
+    @Transactional(readOnly = true)
+    void testRanking() {
 
-    // List<Ranking> res = new ArrayList<Ranking>();
-    // Ranking r1 = statisticsService.getPositionRanking(1L).getSecond();
-    // Ranking r2 = statisticsService.getPositionRanking(2L).getSecond();
-    // Ranking r3 = statisticsService.getPositionRanking(3L).getSecond();
-    // Ranking r4 = statisticsService.getPositionRanking(4L).getSecond();
-    // Ranking r5 = statisticsService.getPositionRanking(5L).getSecond();
-    // Ranking r6 = statisticsService.getPositionRanking(6L).getSecond();
-    // res.add(r2);
-    // res.add(r1);
-    // res.add(r3);
-    // res.add(r5);
-    // res.add(r4);
-    // res.add(r6);
+        List<Ranking> res = new ArrayList<Ranking>();
+        Ranking r1 = statisticsService.getPositionRanking(1L).getSecond();
+        Ranking r2 = statisticsService.getPositionRanking(2L).getSecond();
+        Ranking r3 = statisticsService.getPositionRanking(3L).getSecond();
+        Ranking r4 = statisticsService.getPositionRanking(4L).getSecond();
+        Ranking r5 = statisticsService.getPositionRanking(5L).getSecond();
+        Ranking r6 = statisticsService.getPositionRanking(6L).getSecond();
+        res.add(r2);
+        res.add(r1);
+        res.add(r3);
+        res.add(r5);
+        res.add(r4);
+        res.add(r6);
 
-    // }
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testpropjugadores() {
+    @Test
+    @Transactional(readOnly = true)
+    void testpropjugadores() {
 
-    // Map<Integer, Double> res = new HashMap<Integer, Double>();
-    // res.put(2, 0.0);
-    // res.put(3, 50.0);
-    // res.put(4, 0.0);
-    // res.put(5, 0.0);
-    // res.put(6, 50.0);
-    // res.put(7, 0.0);
-    // res.put(8, 0.0);
+        Map<Integer, Double> res = new HashMap<Integer, Double>();
+        res.put(2, 0.0);
+        res.put(3, 50.0);
+        res.put(4, 0.0);
+        res.put(5, 0.0);
+        res.put(6, 50.0);
+        res.put(7, 0.0);
+        res.put(8, 0.0);
 
-    // assertEquals(statisticsService.getFrecuenciaJugadores(1L), res);
-    // }
+        assertEquals(statisticsService.getFrecuenciaJugadores(1L), res);
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testMaxMinAvgAll() {
-    // Map<String, Double> res = new HashMap<String, Double>();
-    // res.put("min", 0.0);
-    // res.put("avg", 8.666666666666666);
-    // res.put("max", 30.0);
-    // assertEquals(statisticsService.maxMinAvgAll(), res);
+    @Test
+    @Transactional(readOnly = true)
+    void testMaxMinAvgAll() {
+        Map<String, Double> res = new HashMap<String, Double>();
+        res.put("min", 0.0);
+        res.put("avg", 8.666666666666666);
+        res.put("max", 30.0);
+        assertEquals(statisticsService.maxMinAvgAll(), res);
 
-    // }
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testPropTotal() {
-    // assertEquals(statisticsService.propTotal(1L), 0.5);
-    // }
+    @Test
+    @Transactional(readOnly = true)
+    void testPropTotal() {
+        assertEquals(statisticsService.propTotal(1L), 0.5);
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testMaxMinAvgPlayer() {
+    @Test
+    @Transactional(readOnly = true)
+    void testMaxMinAvgPlayer() {
 
-    // Map<String, Double> res = new HashMap<String, Double>();
-    // res.put("min", 10.0);
-    // res.put("avg", 15.0);
-    // res.put("max", 20.0);
-    // System.out.println("HOLAAAAAAAAA");
-    // System.out.println("PRUEBAAAAAAAAAAAAAAA" + statisticsService.maxMinAvg(1L));
+        Map<String, Double> res = new HashMap<String, Double>();
+        res.put("min", 10.0);
+        res.put("avg", 15.0);
+        res.put("max", 20.0);
+        System.out.println("HOLAAAAAAAAA");
+        System.out.println("PRUEBAAAAAAAAAAAAAAA" + statisticsService.maxMinAvg(1L));
 
-    // assertEquals(statisticsService.maxMinAvg(1L), res);
+        assertEquals(statisticsService.maxMinAvg(1L), res);
 
-    // }
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testMaxMinAvgTimePlayer() {
+    @Test
+    @Transactional(readOnly = true)
+    void testMaxMinAvgTimePlayer() {
 
-    // Map<String, Long> res = new HashMap<String, Long>();
-    // res.put("min", 10L);
-    // res.put("avg", 25L);
-    // res.put("max", 40L);
-    // assertEquals(statisticsService.maxMinAvgTime(1L), res);
+        Map<String, Long> res = new HashMap<String, Long>();
+        res.put("min", 10L);
+        res.put("avg", 25L);
+        res.put("max", 40L);
+        assertEquals(statisticsService.maxMinAvgTime(1L), res);
 
-    // }
+    }
 
-    // @Test
-    // @Transactional(readOnly = true)
-    // void testMaxMinAvgTimeAll() {
+    @Test
+    @Transactional(readOnly = true)
+    void testMaxMinAvgTimeAll() {
 
-    // Map<String, Long> res = new HashMap<String, Long>();
-    // res.put("min", 10L);
-    // res.put("avg", 25L);
-    // res.put("max", 40L);
-    // assertEquals(statisticsService.maxMinAvgTimeAll(), res);
+        Map<String, Long> res = new HashMap<String, Long>();
+        res.put("min", 10L);
+        res.put("avg", 25L);
+        res.put("max", 40L);
+        assertEquals(statisticsService.maxMinAvgTimeAll(), res);
 
-    // }
+    }
 
 }
